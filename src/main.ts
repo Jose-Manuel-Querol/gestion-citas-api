f87d3846f3f455287b5a4c36b17a63f8d25883ad
Stepalom77 <stepalom22@gmail.com>
2024-05-09 10:53:05 -0500

    Agregando mas rutas a cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       'http://localhost:4200',
       'http://127.0.0.1:4200',
       'https://gestion-citas-app-production.up.railway.app/',
+      'https://gestion-citas-app-production.up.railway.app',
     ],
     credentials: true,
   });
